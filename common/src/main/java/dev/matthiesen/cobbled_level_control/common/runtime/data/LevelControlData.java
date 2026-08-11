510c9d003b054f6818640b32a7b6c0b69764e619
Adam Matthiesen <amatthiesen@outlook.com>
2026-08-11 13:03:25 -0700

    move player level control functions to LevelControlData and update PlayerExtensions (#24)

--- common/src/main/java/dev/matthiesen/cobbled_level_control/common/runtime/data/LevelControlData.java
+++ common/src/main/java/dev/matthiesen/cobbled_level_control/common/runtime/data/LevelControlData.java
@@ -1,10 +1,130 @@
 package dev.matthiesen.cobbled_level_control.common.runtime.data;
 
+import com.bedrockk.molang.runtime.MoParams;
 import com.cobblemon.mod.common.api.molang.ObjectValue;
+import dev.matthiesen.cobbled_level_control.common.CobbledLevelControl;
+import dev.matthiesen.cobbled_level_control.common.config.CLCConfig;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 public record LevelControlData(Player player, PlayerAccountRecord accountRecord) implements ILevelControlData {
+
+
+    private Object getPlayerStatus(MoParams params) {
+        return accountRecord().asMolangValue();
+    }
+
+    private Object playerLevelUp(MoParams params) {
+        String module = params.getString(0);
+        if (module.isEmpty()) return 0;
+
+        var modInstance = CobbledLevelControl.INSTANCE;
+
+        PlayerAccountRecord playerData = accountRecord != null ? accountRecord : modInstance.getStoredPlayerAccountRecords().getPlayerAccountRecord(player.getUUID());
+        if (playerData == null) return 0;
+
+        int level;
+        int nextLevel;
+
+        switch (module.toLowerCase()) {
+            case "catch" -> {
+                var catchingModule = CLCConfig.getCatchingConfig();
+                level = playerData.getCatching();
+                nextLevel = level + 1;
+                int maxLevel = catchingModule.tiers().size();
+                if (nextLevel > maxLevel) return 0;
+                modInstance.getStoredPlayerAccountRecords().editPlayerAccountRecord(player.getUUID(), record -> record.setCatching(nextLevel));
+                player.sendSystemMessage(Component.literal(
+                        CLCConfig.SERVER_CONFIG.messages_success_targetCatchingTierSet.get()
+                                .replace("%tier%", Integer.toString(nextLevel))
+                ).withStyle(ChatFormatting.AQUA));
+                return 1;
+            }
+            case "level" -> {
+                var levelingModule = CLCConfig.getLevelingConfig();
+                level = playerData.getLeveling();
+                nextLevel = level + 1;
+                int maxLevel = levelingModule.tiers().size();
+                if (nextLevel > maxLevel) return 0;
+                modInstance.getStoredPlayerAccountRecords().editPlayerAccountRecord(player.getUUID(), record -> record.setLeveling(nextLevel));
+                player.sendSystemMessage(Component.literal(
+                        CLCConfig.SERVER_CONFIG.messages_success_targetLevelingTierSet.get()
+                                .replace("%tier%", Integer.toString(nextLevel))
+                ).withStyle(ChatFormatting.AQUA));
+                return 1;
+            }
+            default -> {
+                CobbledLevelControl.INSTANCE.createInfoLog("Invalid module string passed to q.player.level_control().lvlup: " + module);
+                return 0;
+            }
+        }
+    }
+
+    private Object setPlayerLevel(MoParams params) {
+        String module = params.getString(0);
+        int level = params.getInt(1);
+        if (module.isEmpty() || level == 0) return 0;
+
+        var modInstance = CobbledLevelControl.INSTANCE;
+
+        PlayerAccountRecord playerData = accountRecord != null ? accountRecord : modInstance.getStoredPlayerAccountRecords().getPlayerAccountRecord(player.getUUID());
+        if (playerData == null) return 0;
+
+        switch (module.toLowerCase()) {
+            case "catch" -> {
+                var catchingModule = CLCConfig.getCatchingConfig();
+                int maxLevel = catchingModule.tiers().size();
+                if (level > maxLevel) return 0;
+
+                modInstance.getStoredPlayerAccountRecords().editPlayerAccountRecord(player.getUUID(), record -> record.setCatching(level));
+                player.sendSystemMessage(Component.literal(
+                        CLCConfig.SERVER_CONFIG.messages_success_targetCatchingTierSet.get()
+                                .replace("%level%", Integer.toString(level))
+                ).withStyle(ChatFormatting.GREEN));
+                return 1;
+            }
+            case "level" -> {
+                var levelingModule = CLCConfig.getLevelingConfig();
+                int maxLevel = levelingModule.tiers().size();
+                if (level > maxLevel) return 0;
+                modInstance.getStoredPlayerAccountRecords().editPlayerAccountRecord(player.getUUID(), record -> record.setLeveling(level));
+                player.sendSystemMessage(Component.literal(
+                        CLCConfig.SERVER_CONFIG.messages_success_targetLevelingTierSet.get()
+                                .replace("%level%", Integer.toString(level))
+                ).withStyle(ChatFormatting.GREEN));
+                return 1;
+            }
+            default -> {
+                CobbledLevelControl.INSTANCE.createInfoLog("Invalid module string passed to q.player.level_control().setlvl: " + module);
+                return 0;
+            }
+        }
+    }
+
+    private Map<String,? extends Function<MoParams, Object>> getPlayerLevelControlFunctions() {
+        HashMap<String, Function<MoParams, Object>> map = new HashMap<>();
+
+        // q.player.level_control().status() returns following object or 0
+        // { "difficulty": "string", "catching": int, "leveling": int }
+        map.put("status", this::getPlayerStatus);
+
+        // q.player.level_control().lvlup(<module string>) returns 1 for success or 0
+        map.put("lvlup", this::playerLevelUp);
+
+        // q.player.level_control().setlvl(<module string>, <lvl int>) returns 1 for success or 0
+        map.put("setlvl", this::setPlayerLevel);
+
+        return map;
+    }
+
     public ObjectValue<LevelControlData> asMolangValue() {
-        return new ObjectValue<>(this, this::makeString, d -> 1.0);
+        ObjectValue<LevelControlData> value = new ObjectValue<>(this, this::makeString, d -> 1.0);
+        value.functions.putAll(getPlayerLevelControlFunctions());
+        return value;
     }
 }
--- common/src/main/java/dev/matthiesen/cobbled_level_control/common/runtime/molang/PlayerExtensions.java
+++ common/src/main/java/dev/matthiesen/cobbled_level_control/common/runtime/molang/PlayerExtensions.java
@@ -4,134 +4,20 @@ import com.bedrockk.molang.runtime.MoParams;
 import com.cobblemon.mod.common.api.molang.MoLangFunctions;
 import com.cobblemon.mod.common.api.molang.ObjectValue;
 import dev.matthiesen.cobbled_level_control.common.CobbledLevelControl;
-import dev.matthiesen.cobbled_level_control.common.config.CLCConfig;
-import dev.matthiesen.cobbled_level_control.common.runtime.data.PlayerAccountRecord;
 import dev.matthiesen.cobbled_level_control.common.runtime.data.LevelControlData;
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 
 public final class PlayerExtensions {
-    private static Object getPlayerStatus(LevelControlData data) {
-        return data.accountRecord().asMolangValue();
-    }
-
-    private static Object playerLevelUp(MoParams params, Player player, @Nullable LevelControlData data) {
-        String module = params.getString(0);
-        if (module.isEmpty()) return 0;
-
-        var modInstance = CobbledLevelControl.INSTANCE;
-
-        PlayerAccountRecord playerData = data != null ? data.accountRecord() : modInstance.getStoredPlayerAccountRecords().getPlayerAccountRecord(player.getUUID());
-        if (playerData == null) return 0;
-
-        int level;
-        int nextLevel;
-
-        switch (module.toLowerCase()) {
-            case "catch" -> {
-                var catchingModule = CLCConfig.getCatchingConfig();
-                level = playerData.getCatching();
-                nextLevel = level + 1;
-                int maxLevel = catchingModule.tiers().size();
-                if (nextLevel > maxLevel) return 0;
-                modInstance.getStoredPlayerAccountRecords().editPlayerAccountRecord(player.getUUID(), record -> record.setCatching(nextLevel));
-                player.sendSystemMessage(Component.literal(
-                        CLCConfig.SERVER_CONFIG.messages_success_targetCatchingTierSet.get()
-                                .replace("%tier%", Integer.toString(nextLevel))
-                ).withStyle(ChatFormatting.AQUA));
-                return 1;
-            }
-            case "level" -> {
-                var levelingModule = CLCConfig.getLevelingConfig();
-                level = playerData.getLeveling();
-                nextLevel = level + 1;
-                int maxLevel = levelingModule.tiers().size();
-                if (nextLevel > maxLevel) return 0;
-                modInstance.getStoredPlayerAccountRecords().editPlayerAccountRecord(player.getUUID(), record -> record.setLeveling(nextLevel));
-                player.sendSystemMessage(Component.literal(
-                        CLCConfig.SERVER_CONFIG.messages_success_targetLevelingTierSet.get()
-                                .replace("%tier%", Integer.toString(nextLevel))
-                ).withStyle(ChatFormatting.AQUA));
-                return 1;
-            }
-            default -> {
-                CobbledLevelControl.INSTANCE.createInfoLog("Invalid module string passed to q.player.level_control().lvlup: " + module);
-                return 0;
-            }
-        }
-    }
-
-    private static Object setPlayerLevel(MoParams params, Player player, @Nullable LevelControlData data) {
-        String module = params.getString(0);
-        int level = params.getInt(1);
-        if (module.isEmpty() || level == 0) return 0;
-
-        var modInstance = CobbledLevelControl.INSTANCE;
-
-        PlayerAccountRecord playerData = data != null ? data.accountRecord() : modInstance.getStoredPlayerAccountRecords().getPlayerAccountRecord(player.getUUID());
-        if (playerData == null) return 0;
-
-        switch (module.toLowerCase()) {
-            case "catch" -> {
-                var catchingModule = CLCConfig.getCatchingConfig();
-                int maxLevel = catchingModule.tiers().size();
-                if (level > maxLevel) return 0;
-
-                modInstance.getStoredPlayerAccountRecords().editPlayerAccountRecord(player.getUUID(), record -> record.setCatching(level));
-                player.sendSystemMessage(Component.literal(
-                        CLCConfig.SERVER_CONFIG.messages_success_targetCatchingTierSet.get()
-                                .replace("%level%", Integer.toString(level))
-                ).withStyle(ChatFormatting.GREEN));
-                return 1;
-            }
-            case "level" -> {
-                var levelingModule = CLCConfig.getLevelingConfig();
-                int maxLevel = levelingModule.tiers().size();
-                if (level > maxLevel) return 0;
-                modInstance.getStoredPlayerAccountRecords().editPlayerAccountRecord(player.getUUID(), record -> record.setLeveling(level));
-                player.sendSystemMessage(Component.literal(
-                        CLCConfig.SERVER_CONFIG.messages_success_targetLevelingTierSet.get()
-                                .replace("%level%", Integer.toString(level))
-                ).withStyle(ChatFormatting.GREEN));
-                return 1;
-            }
-            default -> {
-                CobbledLevelControl.INSTANCE.createInfoLog("Invalid module string passed to q.player.level_control().setlvl: " + module);
-                return 0;
-            }
-        }
-    }
 
     private static ObjectValue<LevelControlData> buildLevelControlObject(Player player) {
         var modInstance = CobbledLevelControl.INSTANCE;
         var playerData = modInstance.getStoredPlayerAccountRecords().getPlayerAccountRecord(player.getUUID());
         if (playerData == null) return null;
         var data = new LevelControlData(player, playerData);
-        var value = data.asMolangValue();
-        value.functions.putAll(getPlayerLevelControlFunctions(data));
-        return value;
-    }
-
-    private static Map<String,? extends Function<MoParams, Object>> getPlayerLevelControlFunctions(LevelControlData data) {
-        HashMap<String, Function<MoParams, Object>> map = new HashMap<>();
-
-        // q.player.level_control().status() returns following object or 0
-        // { "difficulty": "string", "catching": int, "leveling": int }
-        map.put("status", params -> getPlayerStatus(data));
-
-        // q.player.level_control().lvlup(<module string>) returns 1 for success or 0
-        map.put("lvlup", params -> playerLevelUp(params, data.player(), data));
-
-        // q.player.level_control().setlvl(<module string>, <lvl int>) returns 1 for success or 0
-        map.put("setlvl", params -> setPlayerLevel(params, data.player(), data));
-
-        return map;
+        return data.asMolangValue();
     }
 
     public static void init() {
